2d9a50b52c8b34b45663f1e632d01cc5b3fe621e
wc <3048349591@qq.com>
2024-04-02 18:44:39 +0800

    init

--- .vitepress/cache/deps/@theme_index.js
+++ .vitepress/cache/deps/@theme_index.js
@@ -9,31 +9,31 @@ import {
 } from "./chunk-TV2L3QIC.js";
 
 // node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/index.js
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default6 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default7 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default8 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default9 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default10 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default11 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default12 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default13 } from "D:/booknotes-前端/blog-professional/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default6 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default7 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default8 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default9 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default10 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default11 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default12 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default13 } from "D:/booknotes-前端/vitepress-blog/node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/.pnpm/vitepress@1.0.0-rc.39/node_modules/vitepress/dist/client/shared.js
 var inBrowser = typeof document !== "undefined";
--- vitepress-starter/deploy.sh
+++ vitepress-starter/deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vitepress/dist
+cd vitepress-blog/.vitepress/dist
 
 git init
 git add -A
@@ -15,6 +15,6 @@ git commit -m 'deploy'
 
 # Jacqueline712/mondaylab-blog 替换为自己的用户名和对应的仓库名
 # 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~
-git push -f git@github.com:linlianger/vitepress-blog.git master:gh-pages
+git push -f git@github.com:linlianger/vitepress-blog.git
 
 cd -
